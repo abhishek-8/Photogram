f1504f411cede1cca38961630c0bca004c28d8e3
Abhishek Tiwari <tiwari.abhishek428@gmail.com>
2019-01-01 23:51:45 +0530

    added edit option

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,16 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to(post_path(@post))
+	end
+
 	def create
 		@post = Post.create(post_params)
 		redirect_to posts_path
